9c3fd1308a2847d6c5e2b3f625c785a1b88df731
Gyorgy Fazekas <gyorgyf@dcs.qmul.ac.uk>
2013-06-19 08:27:27 +0100

    funky note value re-estimation using underlying pitch median value

--- src/Analyser.cpp
+++ src/Analyser.cpp
@@ -48,11 +48,11 @@ Analyser::newFileLoaded(Document *doc, WaveFileModel *model,
     m_fileModel = model;
     m_pane = pane;
 
-    // TransformId f0 = "vamp:yintony:yintony:notepitchtrack";
-    // TransformId notes = "vamp:yintony:yintony:notes";
+    TransformId f0 = "vamp:yintony:yintony:notepitchtrack";
+    TransformId notes = "vamp:yintony:yintony:notes";
 
-    TransformId f0 = "vamp:cepstral-pitchtracker:cepstral-pitchtracker:f0";
-    TransformId notes = "vamp:cepstral-pitchtracker:cepstral-pitchtracker:notes";
+    // TransformId f0 = "vamp:cepstral-pitchtracker:cepstral-pitchtracker:f0";
+    // TransformId notes = "vamp:cepstral-pitchtracker:cepstral-pitchtracker:notes";
 
     // We don't want a waveform in the main pane. We must have a
     // main-model layer of some sort, but the layers created by
