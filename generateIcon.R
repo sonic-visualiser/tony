bbb6f4cb50873c21db91aea2fc9dfbcfc2a1bb65
Matthias Mauch <matthiasmauch@gmail.com>
2013-11-29 14:54:36 +0000

    added white halo to pitchtrack in icon

--- generateIcon.R
+++ generateIcon.R
@@ -5,6 +5,7 @@ pitchDiff <- 6
 sin1 <- sin(t) + pitchDiff
 sin2 <- sin(t)
 
+pad <- 3
 lineWidth <- 15
 outlineWidth <- 20
 sz <- 200
@@ -14,7 +15,10 @@ par(mar=c(0,0,0,0))
 plot(t, sin1 * w + sin2 * (1-w), type='l', 
      lwd=lineWidth, bty='n', xaxt='n', yaxt='n',
      ylim = c(-pad, pitchDiff+pad) + c(1,-1)*0.1,
-     xlim = c(0, 8*pi) + c(1,-1)*0.1)
+     xlim = c(0, 8*pi) + c(1,-1)*0.1,
+     col="white")
+plot(t, sin1 * w + sin2 * (1-w), type='l', 
+     lwd=lineWidth, col = blacj)
 lines(c(0,0),c(-pad,pitchDiff+pad), lwd=outlineWidth, col = 'blue', lend=2)
 lines(c(0,0)+8*pi,c(-pad,pitchDiff+pad), lwd=outlineWidth, col = 'blue', lend=2)
 lines(c(0,8*pi),c(0,0)+pitchDiff+pad, lwd=outlineWidth, col = 'blue', lend=2)
