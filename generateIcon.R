0a9da2d04c8912f0a008c49a2ab1f4b534d05d84
Matthias Mauch <matthiasmauch@gmail.com>
2013-12-01 14:33:55 +0000

    white background to pitchtrack in icon

--- generateIcon.R
+++ generateIcon.R
@@ -4,6 +4,7 @@ n <- length(t)
 pitchDiff <- 6
 sin1 <- sin(t) + pitchDiff
 sin2 <- sin(t)
+x <- sin1 * w + sin2 * (1-w)
 
 pad <- 3
 lineWidth <- 15
@@ -12,13 +13,13 @@ sz <- 200
 
 png("tonyLogoWave.png", width = sz, height = sz, bg="transparent")
 par(mar=c(0,0,0,0))
-plot(t, sin1 * w + sin2 * (1-w), type='l', 
-     lwd=lineWidth, bty='n', xaxt='n', yaxt='n',
+plot(t, x, type='l', 
+     lwd=lineWidth*3, bty='n', xaxt='n', yaxt='n',
      ylim = c(-pad, pitchDiff+pad) + c(1,-1)*0.1,
      xlim = c(0, 8*pi) + c(1,-1)*0.1,
      col="white")
-plot(t, sin1 * w + sin2 * (1-w), type='l', 
-     lwd=lineWidth, col = blacj)
+lines(t, x, 
+     lwd=lineWidth, col = "black")
 lines(c(0,0),c(-pad,pitchDiff+pad), lwd=outlineWidth, col = 'blue', lend=2)
 lines(c(0,0)+8*pi,c(-pad,pitchDiff+pad), lwd=outlineWidth, col = 'blue', lend=2)
 lines(c(0,8*pi),c(0,0)+pitchDiff+pad, lwd=outlineWidth, col = 'blue', lend=2)
