30ce63b88d332faea1b4e4fb782e8024cd12462c
Matthias Mauch <matthiasmauch@gmail.com>
2013-12-03 17:06:23 +0000

    changed icon code to directly generate the pngs needed (for linux?)

--- generateIcon.R
+++ generateIcon.R
@@ -7,21 +7,23 @@ sin2 <- sin(t)
 x <- sin1 * w + sin2 * (1-w)
 
 pad <- 3
-lineWidth <- 15
-outlineWidth <- 20
-sz <- 200
 
-png("tonyLogoWave.png", width = sz, height = sz, bg="transparent")
-par(mar=c(0,0,0,0))
-plot(t, x, type='l', 
-     lwd=lineWidth*3, bty='n', xaxt='n', yaxt='n',
-     ylim = c(-pad, pitchDiff+pad) + c(1,-1)*0.1,
-     xlim = c(0, 8*pi) + c(1,-1)*0.1,
-     col="white")
-lines(t, x, 
-     lwd=lineWidth, col = "black")
-lines(c(0,0),c(-pad,pitchDiff+pad), lwd=outlineWidth, col = 'blue', lend=2)
-lines(c(0,0)+8*pi,c(-pad,pitchDiff+pad), lwd=outlineWidth, col = 'blue', lend=2)
-lines(c(0,8*pi),c(0,0)+pitchDiff+pad, lwd=outlineWidth, col = 'blue', lend=2)
-lines(c(0,8*pi),c(0,0)-pad, lwd=outlineWidth, col = 'blue', lend=2)
-dev.off()
+for (sz in c(16,22,24,32,48,64,128)) {
+  lineWidth <- 15 * sz/200
+  outlineWidth <- 20 * sz/200
+  png(sprintf("~/code/tonioni/icons/tony-%ix%i.png",sz,sz), width = sz, height = sz, bg="transparent")
+  par(mar=c(0,0,0,0))
+  plot(t, x, type='l', 
+       lwd=lineWidth*3, bty='n', xaxt='n', yaxt='n',
+       ylim = c(-pad, pitchDiff+pad) + c(1,-1)*0.1,
+       xlim = c(0, 8*pi) + c(1,-1)*0.1,
+       col="white")
+  lines(t, x, 
+        lwd=lineWidth, col = "black")
+  lines(c(0,0),c(-pad,pitchDiff+pad), lwd=outlineWidth, col = 'blue', lend=2)
+  lines(c(0,0)+8*pi,c(-pad,pitchDiff+pad), lwd=outlineWidth, col = 'blue', lend=2)
+  lines(c(0,8*pi),c(0,0)+pitchDiff+pad, lwd=outlineWidth, col = 'blue', lend=2)
+  lines(c(0,8*pi),c(0,0)-pad, lwd=outlineWidth, col = 'blue', lend=2)
+  dev.off()
+}
+
