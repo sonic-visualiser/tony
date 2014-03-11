87cb07eaab6e5baba2b6d070d9d5fe23ec71c580
Chris Cannam <cannam@all-day-breakfast.com>
2014-03-11 10:01:27 +0000

    Archiver for source packaging

---
+++ deploy/src/archive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+tag=`hg tags | grep '^v[0-9]' | head -1 | awk '{ print $1; }'`
+
+v=`echo "$tag" |sed 's/v//'`
+
+if test -z "$v" ; then
+    echo "No suitable tag found!?"
+    exit 1
+fi
+
+echo "Packaging up version $v from tag $tag..."
+
+hg archive -r"$tag" --subrepos --exclude sv-dependency-builds --exclude testdata /tmp/tony-"$v".tar.gz
+
