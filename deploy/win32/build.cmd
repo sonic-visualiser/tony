d93668acf0deb2103e72c2586e9010d13bdccba4
Chris Cannam <cannam@all-day-breakfast.com>
2014-06-12 17:23:05 +0100

    Win32 build updates for Qt5.3 build

--- deploy/win32/build.cmd
+++ deploy/win32/build.cmd
@@ -1,11 +1,16 @@
 @ECHO OFF
-SET WIXPATH="C:\Program Files (x86)\WiX Toolset v3.7\bin"
+SET WIXPATH="C:\Program Files (x86)\WiX Toolset v3.8\bin"
 IF NOT EXIST %WIXPATH% (
-    SET WIXPATH="C:\Program Files\WiX Toolset v3.7\bin"
+    SET WIXPATH="C:\Program Files\WiX Toolset v3.8\bin"
 )
-DEL tony.msi
-%WIXPATH%\candle.exe -v tony.wxs
-%WIXPATH%\light.exe -b ..\.. -ext WixUIExtension -v tony.wixobj
-PAUSE
-DEL tony.wixobj
-DEL tony.wixpdb
+IF NOT EXIST %WIXPATH% (
+    ECHO "WiX not found, exiting"
+)
+IF EXIST %WIXPATH% (
+    DEL tony.msi
+    %WIXPATH%\candle.exe -v tony.wxs
+    %WIXPATH%\light.exe -b ..\.. -ext WixUIExtension -v tony.wixobj
+    PAUSE
+    DEL tony.wixobj
+    DEL tony.wixpdb
+)
