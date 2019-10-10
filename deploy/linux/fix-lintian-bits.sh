e3b3e888e4c3a4e38a95b922c79a66fe49163422
Chris Cannam <cannam@all-day-breakfast.com>
2019-10-10 14:23:28 +0100

    Ah... Don't strip non-binaries

--- deploy/linux/fix-lintian-bits.sh
+++ deploy/linux/fix-lintian-bits.sh
@@ -7,7 +7,7 @@ dir=$1
 set -eu
 
 strip "$dir"/usr/bin/*
-strip "$dir"/usr/lib/*/*
+strip "$dir"/usr/lib/*/*.so
 
 sz=`du -sx --exclude DEBIAN "$dir" | awk '{ print $1; }'`
 perl -i -p -e "s/Installed-Size: .*/Installed-Size: $sz/" "$dir"/DEBIAN/control
