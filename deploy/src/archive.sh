2b6f72afbe0a15e6928ac5866f52895bbc68377e
Chris Cannam <cannam@all-day-breakfast.com>
2019-10-10 15:44:47 +0100

    Omit testdata from source archive

--- deploy/src/archive.sh
+++ deploy/src/archive.sh
@@ -18,7 +18,7 @@ echo -n "Packaging up version $v from tag $tag... "
 
 hg update -r"$tag"
 
-./repoint archive /tmp/tony-"$v".tar.gz --exclude sv-dependency-builds repoint.pri
+./repoint archive /tmp/tony-"$v".tar.gz --exclude sv-dependency-builds repoint.pri testdata
 
 hg update -r"$current"
 
