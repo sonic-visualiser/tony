34fb6151b5ab16f982bd04634569e214916d8b3c
Chris Cannam <cannam@all-day-breakfast.com>
2019-10-18 14:19:36 +0100

    Single script to rebuild, package, and copy properly named package to packages directory

---
+++ deploy/clean-build-and-package.bat
@@ -0,0 +1,25 @@
+@rem  Run this from within the top-level dir: deploy\clean-build-and-package
+@echo on
+
+@set /p VERSION=<version.h
+@set VERSION=%VERSION:#define TONY_VERSION "=%
+set VERSION=%VERSION:"=%
+
+@echo ""
+@set YN=y
+@set /p YN="Proceed to clean, rebuild, package, and sign version %VERSION% [Yn] ?"
+
+@if "%YN%" == "Y" set YN=y
+@if "%YN%" neq "y" exit /b 3
+
+@echo Proceeding
+call .\deploy\win64\build-and-package.bat sign
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+mkdir packages
+copy build_win64\tony.msi packages\tony-%VERSION%-win64.msi
+
+@echo ""
+@echo Done
+
+   
--- deploy/win64/build-and-package.bat
+++ deploy/win64/build-and-package.bat
@@ -66,5 +66,6 @@ signtool verify /pa tony.msi
 
 set PATH=%ORIGINALPATH%
 
+cd %STARTPWD%
 @echo Done
 
