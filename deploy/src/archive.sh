8c9a27c384073beea2139a3db5901be9492d7eb8
Chris Cannam <cannam@all-day-breakfast.com>
2019-10-25 11:24:37 +0100

    Place source package in packages/

--- deploy/src/archive.sh
+++ deploy/src/archive.sh
@@ -18,7 +18,7 @@ echo -n "Packaging up version $v from tag $tag... "
 
 hg update -r"$tag"
 
-./repoint archive /tmp/tony-"$v".tar.gz --exclude sv-dependency-builds repoint.pri testdata pyin/testdata
+./repoint archive "$(pwd)"/packages/tony-"$v".tar.gz --exclude sv-dependency-builds repoint.pri testdata pyin/testdata
 
 hg update -r"$current"
 
